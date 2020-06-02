1f0f5092990f9e5e018b43af116f1cc404545d16
pranay reddy <kpranayreddy2000@gmail.com>
2020-06-02 13:52:44 +0530

    check

--- src/routers/eventDashboard.js
+++ src/routers/eventDashboard.js
@@ -2,7 +2,7 @@ const express=require('express')
 const auth=require('../middleware/auth')
 const Upload=require('../models/upload')
 module.exports=(app,io)=>{
-    app.get('/home',auth,async(req,res)=>{
+    app.get('/i',auth,async(req,res)=>{
         try{
             const event=await Upload.find()
             if(!event){
